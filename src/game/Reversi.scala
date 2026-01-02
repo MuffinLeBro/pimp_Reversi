81e573fc8b9d9b89aa5b0c68dad9c9e36424c84b
ravaka <ravknas93@gmail.com>
2026-01-03 00:20:56 +0100

    FEATURE - player has valid moves for his turn

--- src/game/Reversi.scala
+++ src/game/Reversi.scala
@@ -7,4 +7,15 @@ object Reversi extends App{
   var game: Game = new Game()
   game.askPlayerName()
   game.init_game()
+  game.current_player = game.players(0) // set the first player who plays
+
+  do{
+     if(game.current_player.can_play(game.board)){
+        println("CAN PLAY")
+     }
+    else{
+      game.switch_player()
+    }
+  }while(!game.isOver)
 }
+
