a8eb79427c9a37bbae7a663cbe6ddf0cb228dfe6
ravaka <ravknas93@gmail.com>
2026-01-06 01:24:47 +0100

    FEATURE - (CODE INIT) add mouse listener - ADD audio files - apply color changing after click

---
+++ src/classes/Audio.scala
@@ -0,0 +1,47 @@
+package classes
+
+import javax.sound.sampled.{AudioSystem, Clip}
+
+class Audio(path: String){
+  var audioClip: Clip = _
+
+  try {
+    val url = classOf[Audio].getResource(path)
+    println(url)
+    val audioStream = AudioSystem.getAudioInputStream(url)
+
+    audioClip = AudioSystem.getClip.asInstanceOf[Clip]
+    audioClip.open(audioStream)
+  } catch {
+    case e: Exception =>
+      e.printStackTrace()
+  }
+
+  def play(): Unit = {
+    // Open stream and play
+    try {
+      if (!audioClip.isOpen) audioClip.open()
+      audioClip.stop()
+      if(path == "/sounds/tictoc.wav"){
+        audioClip.loop(Clip.LOOP_CONTINUOUSLY) // to loop a sound
+      }
+      audioClip.start()
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+  }
+
+  def stop(): Unit = {
+    // Open stream and play
+    try {
+      if (audioClip.isOpen) {
+        audioClip.stop()
+        audioClip.setFramePosition(0); // important if we want to do a stop and play after action
+      }
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+  }
+}
--- src/trait/Config.scala
+++ src/trait/Config.scala
@@ -1,13 +1,17 @@
 package `trait`
 
-import java.awt.Color
+import java.awt.{Color, Font}
 
 trait Config {
   val RADIUS: Int = 50
   val CELL_DIMENSION: Int = 100
-  val MARGIN: Int = 50
+  val MARGIN: Int = 70
   val PADDING: Int = 10
-  val GREEN: Color = new Color(54, 95,38)
-  val LINE_COLOR = new Color(40, 72,29)
-  val BACKGROUND = new Color(245, 237,221)
+
+  val GREEN: Color = new Color(54, 95, 38)
+  val LINE_COLOR = new Color(40, 72, 29)
+  val BACKGROUND = new Color(76, 75, 75)
+
+  val FONT_SCORE = new Font("Consolas", Font.BOLD, 30)
+  val FONT_NAME = new Font("Arial", Font.BOLD, 22)
 }
