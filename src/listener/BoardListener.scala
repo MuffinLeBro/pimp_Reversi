de08a53b00f082efe32a1f9929fa47a8d6038787
ravaka <ravknas93@gmail.com>
2026-01-10 11:42:28 +0100

    FEATURE ADD SCREEN LOADER: add screen loader - modify the interface of game - add button RESTART/REPLAY/QUIT

--- src/listener/BoardListener.scala
+++ src/listener/BoardListener.scala
@@ -10,44 +10,56 @@ class BoardListener(game: Game) extends MouseAdapter with Config{
 
   override def mouseClicked(e: MouseEvent): Unit = {
 
-    if(!game.click_enable) return
+    if(game.isStarted){
+      if(!game.click_enable) return
+      else{
+        val event = e
+        // Get the mouse position from the event
+        val posx = event.getX
+        val posy = event.getY
+        if(posx >= MARGIN && posx <= game.board.BOARD_WIDTH && posy >= MARGIN && posy <= game.board.BOARD_HEIGHT){
+          for(i <- game.board.playBoard.indices){
+            for(j <- game.board.playBoard(i).indices){
+              if(game.board.playBoard(i)(j).pion.color == GREEN && (Math.abs(posx - game.board.playBoard(i)(j).center.getX) <= CELL_DIMENSION / 2) && (Math.abs(posy - game.board.playBoard(i)(j).center.getY) <= CELL_DIMENSION / 2)){
+                if(game.board.isValidMove(game.current_player, i, j)){
+                  game.board.applyMove(game.display, game.current_player, i, j)
+                  game.number_of_switch = 0
 
-    else{
-      val event = e
-      // Get the mouse position from the event
-      val posx = event.getX
-      val posy = event.getY
-      if(posx >= MARGIN && posx <= game.board.BOARD_WIDTH && posy >= MARGIN && posy <= game.board.BOARD_HEIGHT){
-        for(i <- game.board.playBoard.indices){
-          for(j <- game.board.playBoard(i).indices){
-            if(game.board.playBoard(i)(j).pion.color == GREEN && (Math.abs(posx - game.board.playBoard(i)(j).center.getX) <= CELL_DIMENSION / 2) && (Math.abs(posy - game.board.playBoard(i)(j).center.getY) <= CELL_DIMENSION / 2)){
-              if(game.board.isValidMove(game.current_player, i, j)){
-                game.disableClick()
-                game.board.applyMove(game.display, game.current_player, i, j)
+                  game.disableClick()
+                  game.updateScore()
+                  game.switch_player()
+                  game.startTurn()
 
-                game.number_of_switch = 0
-                game.updateScore()
-                game.switch_player()
-                game.startTurn()
-
-                // Manage the sound of good move
-                game.audio_good.play()
-                Thread.sleep(1000)
-                if(game.audio_good.audioClip.isRunning) game.audio_good.stop()
-
-                game.updateTurn(game.players.indexOf(game.current_player))
-                game.display.syncGameLogic(60)
-              }
-              else{
-                game.startTurn()
+                  // Manage the sound of good move
+                  game.audio_good.play()
+                  Thread.sleep(1000)
+                  if(game.audio_good.audioClip.isRunning) game.audio_good.stop()
+                  game.updateTurn(game.players.indexOf(game.current_player))
+                }
+                else{
+                  game.startTurn()
+                }
               }
             }
           }
         }
-      }
-      else{
-        game.startTurn()
+        else{
+          if(posx >= CLICK_REPLAY_RESTART_REPLAY._1 && posx <= (CLICK_REPLAY_RESTART_REPLAY._1 + 150) && posy >= CLICK_REPLAY_RESTART_REPLAY._2 && posy <= (CLICK_REPLAY_RESTART_REPLAY._2 + 70)) game.restart()
+          if(posx >= CLICK_QUIT._1 && posx <= (CLICK_QUIT._1 + 150) && posy >= CLICK_QUIT._2 && posy <= (CLICK_QUIT._2 + 70)) System.exit(0)
+        }
       }
     }
+    else{
+      val event = e
+      val posx = event.getX
+      val posy = event.getY
+
+      if(posx >= POSITION_BUTTON_PLAY._1 - 150 && posx <= (POSITION_BUTTON_PLAY._1 - 150 + 300) && posy >= POSITION_BUTTON_PLAY._2 - 50 && posy <= (POSITION_BUTTON_PLAY._2 - 50 + 100)){
+        game.isStarted = true
+        game.start()
+        game.audio_intro.stop()
+      }
+    }
+    game.display.syncGameLogic(60)
   }
 }
--- src/trait/Config.scala
+++ src/trait/Config.scala
@@ -1,6 +1,7 @@
 package `trait`
 
 import java.awt.{Color, Font}
+import java.io.File
 
 trait Config {
   val RADIUS: Int = 50
@@ -8,12 +9,27 @@ trait Config {
   val MARGIN: Int = 70
   val PADDING: Int = 10
 
-  val GREEN: Color = new Color(79, 152, 46)
-//  val GREEN: Color = new Color(54, 95, 38)
-  val LINE_COLOR = new Color(40, 72, 29)
-  val BACKGROUND = new Color(64, 125, 35)
-//  val BACKGROUND = new Color(76, 75, 75)
+//  val GREEN: Color = new Color(79, 152, 46)
+  val BLACK: Color = new Color(13, 13, 13)
+  val GREEN: Color = new Color(54, 95, 38)
+//  val LINE_COLOR = new Color(40, 72, 29)
+  val LINE_COLOR = Color.BLACK
+  val BACKGROUND = new Color(67, 130, 60)
 
   val FONT_SCORE = new Font("Consolas", Font.BOLD, 30)
   val FONT_NAME = new Font("Arial", Font.BOLD, 22)
+
+  // Load the font from a file
+  private val audiowide = new File("src/font/Audiowide-Regular.ttf")
+  private val lobster = new File("src/font/Lobster-Regular.ttf")
+  val CUSTOM_FONT_AUDIOWIDE: Font = Font.createFont(Font.TRUETYPE_FONT, audiowide)
+  val CUSTOM_FONT_LOBSTER: Font = Font.createFont(Font.TRUETYPE_FONT, lobster)
+
+  val POSITION_BUTTON_PLAY: (Int, Int) = (200, 650)
+  val POSITION_PION: (Int, Int) = (650, 550)
+  val POSITION_TEXT_PLAY: (Int, Int) = (130, 670)
+  val POSITION_TEXT_QUIT: (Int, Int) = (1030, 810)
+  val POSITION_TEXT_RESTART_REPLAY: (Int, Int) = (1045, 710)
+  val CLICK_REPLAY_RESTART_REPLAY: (Int, Int) = (1025, 665)
+  val CLICK_QUIT: (Int, Int) = (1025, 765)
 }
