6c9f343a5cc7776f0f8e940aef3263891afff4ea
ravaka <ravknas93@gmail.com>
2026-01-10 11:45:13 +0100

    FEATURE ADD SCREEN LOADER: Code correction

--- src/trait/Config.scala
+++ src/trait/Config.scala
@@ -11,15 +11,15 @@ trait Config {
 
   val BLACK: Color = new Color(13, 13, 13)
   val GREEN: Color = new Color(54, 95, 38)
-  val LINE_COLOR = Color.BLACK
+  val LINE_COLOR: Color = Color.BLACK
   val BACKGROUND = new Color(67, 130, 60)
 
   val FONT_SCORE = new Font("Consolas", Font.BOLD, 30)
   val FONT_NAME = new Font("Arial", Font.BOLD, 22)
 
   // Load the font from a file
-  private val audiowide = new File("src/font/Audiowide-Regular.ttf")
-  private val lobster = new File("src/font/Lobster-Regular.ttf")
+  private val audiowide: File = new File("src/font/Audiowide-Regular.ttf")
+  private val lobster: File = new File("src/font/Lobster-Regular.ttf")
   val CUSTOM_FONT_AUDIOWIDE: Font = Font.createFont(Font.TRUETYPE_FONT, audiowide)
   val CUSTOM_FONT_LOBSTER: Font = Font.createFont(Font.TRUETYPE_FONT, lobster)
 
